2141c809c33d699e25e82ae18eb950bd3bcc6888
kyler-lfx <38668767+kyler-lfx@users.noreply.github.com>
2022-12-07 08:27:07 -0800

    st

--- src/components/contact/contact-form.jsx
+++ src/components/contact/contact-form.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import './contact.css'
 import { BsFillArrowRightCircleFill, BsFillArrowRightSquareFill  } from 'react-icons/bs'
 
